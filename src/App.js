83cd77c9af49b46d69087c3597926e4fd76e6aad
josepht96 <Joe.Thomas@allscripts.com>
2021-03-02 17:02:29 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import NavBar from './components/NavBar';
+import Home from './pages/home'
+import About from './pages/about'
+import React from './pages/reactbasic'
 import './App.css';
+
+const isActive = {
+  fontWeight: "bold",
+  backgroundColor: "rgba(255, 255, 255, 0.1)",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="SidePanel">
+          <NavBar />
+        </div>
+        <div className="MainBody">
+          <Switch>
+            <Route path='/x' component={Home} />
+            <Route path='/asbout' component={About} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Toggle from "./pages/toggle";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
